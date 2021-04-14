cbd6647cf77198f1d7cdba7c5e08e8c6194976ef
khaled611 <>
2021-04-14 12:53:44 +0000

    fight club js

---
+++ app.js
@@ -0,0 +1,8 @@
+var yourAge= prompt('please enter your age')
+if (yourAge >=18)
+{alert ('enjoy watching')
+}
+else{
+  alert('sorry this  movie is not good for you')
+}
+document.write('<img src="http://lucyvhayauthor.com/wp-content/uploads/2017/02/689300.jpg">')
